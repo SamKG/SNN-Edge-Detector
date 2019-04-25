e470446ae48d47b0ed63b4eb495adb4f63b57006
SamKG <samyak.k.gupta@gmail.com>
2019-04-25 16:01:37 -0400

    brian2 file loading

--- vectorized/bipolar_layer.py
+++ vectorized/bipolar_layer.py
@@ -3,12 +3,12 @@ from brian2 import *
 from brian2tools import *
 
 OnCenterOffSurround = NeuronGroup(NUM_NEURONS, eqs, threshold='v > 10*mV', reset='v = 0*mV',
-                    refractory=5*ms, method='exact')
+                    refractory=REFRACTORY_TIME, method=INTEG_METHOD)
 OnCenterOffSurround.v = 0*mV # initialize voltages to 0
 OnCenterOffSurround.a = 0
 
 OffCenterOnSurround = NeuronGroup(NUM_NEURONS, eqs, threshold='v > 10*mV', reset='v = 0*mV',
-                    refractory=5*ms, method='exact')
+                    refractory=REFRACTORY_TIME, method=INTEG_METHOD)
 OffCenterOnSurround.v = 0*mV # initialize voltages to 0
 OffCenterOnSurround.a = 0
 
